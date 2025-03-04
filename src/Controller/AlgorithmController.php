42b8f95f54f5b3a808ea40969a89a7982d4c8dd7
Marino2004 <marinomiha7@gmail.com>
2025-03-04 12:37:53 +0300

    reformated code

--- src/Controller/AlgorithmController.php
+++ src/Controller/AlgorithmController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Algorithm;
 use App\Form\AlgorithmType;
 use App\Service\AlgorithmService;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,14 +10,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CreateController extends AbstractController
+class AlgorithmController extends AbstractController
 {
     public function __construct(
         private readonly AlgorithmService $algorithmService,
     ) { }
 
     #[Route('/create', name: 'app_create')]
-    public function index(Request $request): Response
+    public function create(Request $request): Response
     {
         $form = $this
             ->createForm(AlgorithmType::class)
@@ -38,6 +39,35 @@ class CreateController extends AbstractController
         
         return $this->render('algorithm/create.html.twig', [
             "form" => $form,
+        ]);
+    }
+
+    #[Route('/get', name: 'app_get')]
+    public function read(): Response
+    {
+        $result = $this->algorithmService->findAllAlgorithms();
+
+        return $this->render('algorithm/get.html.twig', [
+            'result' => $result,
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'app_show')]
+    public function show(Algorithm $algorithm): Response
+    {
+        return $this->render('algorithm/show.html.twig', [
+            'algorithm' => $algorithm
+        ]);
+    }
+
+    #[Route('{id}/export', name: 'app_pdf')]
+    public function export(Algorithm $algorithm)
+    {
+        $pdfContent = $this->algorithmService->exportToPdf($algorithm);
+
+        return new Response($pdfContent, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="algo.pdf"',
         ]);
     }
 }
